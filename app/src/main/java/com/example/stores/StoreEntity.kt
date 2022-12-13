844b5dc6e4b9b3940863442ed3a2a5c9d339764a
Pedro Molina <pedro.molina@petroprix.com>
2022-12-13 19:15:49 +0100

    terminada

--- app/src/main/java/com/example/stores/StoreEntity.kt
+++ app/src/main/java/com/example/stores/StoreEntity.kt
@@ -11,4 +11,19 @@ data class StoreEntity(
     var website: String = "",
     var photoUrl:String,
     var isFavorite: Boolean = false
-)
+){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as StoreEntity
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
